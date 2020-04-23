b3b5b5fae0ca41978dd7229fb4406143dfcfd594
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-04-23 10:48:58 +0300

    added code to data clean file

--- data_clean.py
+++ data_clean.py
@@ -21,10 +21,31 @@ df_test = pd.read_csv("test.csv")
 import numpy as np
 import matplotlib.pyplot as plt
 
-class_type = df_train['Pclass']
-age = df_train['Age']
+#for each Pclass(3) find the mean of female age and male age.
+#for each missing data(nan) in 'Age' fill in age according to
+#the sex type and it's mean
 
-plt.figure(figsize=(9, 3))
-plt.subplot(131)
-plt.bar(class_type, age)
-plt.show()
+df_train_pclass = df_train[['Pclass', 'Sex','Age']] #new dataframe with relevent columns
+#s_sex = df_train['Sex'].apply(lambda x: 1 if x.lower() == 'male' else 0) #transfroming 'male' & 'female' to 1 & 0
+df = df_train_pclass.copy()
+#df = df.assign(Sex = s_sex)
+
+#time to use or copy of df_train_pclass dataframe:
+#We need to get a certain pclass group(1,2,3), and find the age mean of the male group(1) and female group(0)
+
+#creating dataframes with the specific pclass value:
+df_pclas_first = df[df['Pclass']==1]
+df_pclass_second = df[df['Pclass']==2]
+df_pclass_third = df[df['Pclass'] ==3]
+
+#finding the mean of Age of each pclass df according to sex value(male/female)
+
+
+
+
+#dummy = pd.get_dummies(df_train_pclass['Sex']) #setting dummy variable
+#df_train_pclass = pd.concat([df_train_pclass, dummy], axis=1)
+#df_train_pclass.drop(['Sex'], axis=1, inplace=True)
+
+        
+
