dee95e875075a61d08c7422cc637c8e6d17c2621
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-04-26 01:24:20 +0300

    added new code
    found the avg age of both male and female pssngs from all classes

--- data_clean.py
+++ data_clean.py
@@ -18,34 +18,34 @@ df_test = pd.read_csv("test.csv")
 
 
 ###df_train cleaning
-import numpy as np
-import matplotlib.pyplot as plt
 
 #for each Pclass(3) find the mean of female age and male age.
 #for each missing data(nan) in 'Age' fill in age according to
 #the sex type and it's mean
 
-df_train_pclass = df_train[['Pclass', 'Sex','Age']] #new dataframe with relevent columns
-#s_sex = df_train['Sex'].apply(lambda x: 1 if x.lower() == 'male' else 0) #transfroming 'male' & 'female' to 1 & 0
-df = df_train_pclass.copy()
-#df = df.assign(Sex = s_sex)
+df_train_pclass = df_train[['Pclass', 'Sex','Age']]
 
-#time to use or copy of df_train_pclass dataframe:
-#We need to get a certain pclass group(1,2,3), and find the age mean of the male group(1) and female group(0)
+#pclass = 1
+filt = df_train_pclass[(df_train_pclass['Pclass']==1) & (df_train_pclass['Sex'] == 'female')]
+female_1class_mean = int(filt['Age'].mean())
 
-#creating dataframes with the specific pclass value:
-df_pclas_first = df[df['Pclass']==1]
-df_pclass_second = df[df['Pclass']==2]
-df_pclass_third = df[df['Pclass'] ==3]
+filt = df_train_pclass[(df_train_pclass['Pclass']==1) & (df_train_pclass['Sex'] == 'male')]
+male_1class_mean = int(filt['Age'].mean())
 
-#finding the mean of Age of each pclass df according to sex value(male/female)
+#pclass = 2
+filt = df_train_pclass[(df_train_pclass['Pclass']==2) & (df_train_pclass['Sex'] == 'female')]
+female_2class_mean = int(filt['Age'].mean())
 
+filt = df_train_pclass[(df_train_pclass['Pclass']==2) & (df_train_pclass['Sex'] == 'male')]
+male_2class_mean = int(filt['Age'].mean())
 
+#pclass = 3
+filt = df_train_pclass[(df_train_pclass['Pclass']==3) & (df_train_pclass['Sex'] == 'female')]
+female_3class_mean = int(filt['Age'].mean())
 
+filt = df_train_pclass[(df_train_pclass['Pclass']==3) & (df_train_pclass['Sex'] == 'male')]
+male_3class_mean = int(filt['Age'].mean())
 
-#dummy = pd.get_dummies(df_train_pclass['Sex']) #setting dummy variable
-#df_train_pclass = pd.concat([df_train_pclass, dummy], axis=1)
-#df_train_pclass.drop(['Sex'], axis=1, inplace=True)
-
-        
-
+#fill the missing Age data 
+df = df_train.copy()
+df = df_train.apply(lambda x: female_1class_mean if x['Pclass']==1 & x['Sex'].lower()=='female' & isinstance(x['Age'],int) else x['Age'])
