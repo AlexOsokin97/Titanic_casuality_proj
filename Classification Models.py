f1b96fc5f5a1b9eabf1b64edc4a43c9f520a6116
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-06-15 08:42:41 +0300

    Update Classification Models.py

--- Classification Models.py
+++ Classification Models.py
@@ -23,61 +23,66 @@ X = df_dums.drop(['Survived','Sex_male', 'Embarked_Southampton'], axis=1).values
 y = df_dums['Survived'].values
 
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 #creating train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 ######################################GradientBoostingClassifier###########################################
-
 from sklearn.ensemble import GradientBoostingClassifier
 
-gbc = GradientBoostingClassifier(loss='deviance', learning_rate=0.01, 
-                                 n_estimators=1000, criterion='friedman_mse')
+gbc = GradientBoostingClassifier(random_state=42)
+print("Avg GradientBoosting accuracy: ", np.mean(cross_val_score(gbc, X_train, y_train, cv=5)), "%")
 
-gbc.fit(X_train, y_train)
+gbc_params = [{'loss':('deviance','exponential'), 'learning_rate': (0.0001, 0.001, 0.01, 0.1), 'n_estimators': range(100,1500,100),
+               'criterion':('friedman_mse', 'mse', 'mae'), 'min_samples_split': range(2,10,2), 'min_samples_leaf': range(2,12,2),
+               'max_features': ('auto', 'sqrt', 'log2', None)}]
+gbc_gs = GridSearchCV(gbc, gbc_params, scoring='accuracy', cv=3)
 
-y_pred_gb = gbc.predict(X_test)
-cm_gb = confusion_matrix(y_test, y_pred_gb)
-print(cm_gb)
-print("Accuracy: {}%".format(accuracy_score(y_test, y_pred_gb)))
+gbc_gs.fit(X_train, y_train)
 
+gbc_score = gbc_gs.best_score_
+gbc_param = gbc_gs.best_params_
+gbc_estimator = gbc_gs.best_estimator_
 
-gb_accuracies = cross_val_score(gbc, X = X_train, y = y_train, cv=7)
-print("The Avg Accuracy of cross-val-score: {}%".format(gb_accuracies.mean()*100))
+print("Best Score: ", gbc_gs.best_score_)
+print("Best parameters: ", gbc_gs.best_params_)
+print("Best model(estimator): ", gbc_gs.best_estimator_)
 
 #########################################XGBClassifier#########################################
 
 from xgboost import XGBClassifier
 
-xgb = XGBClassifier()
+xgb = XGBClassifier(random_state=42)
+print("Avg XGBoosting accuracy: ", np.mean(cross_val_score(xgb, X_train, y_train, cv=5)), "%")
 
-xgb.fit(X_train, y_train)
+xgb_params = [{             }]
 
-y_pred_XGB = xgb.predict(X_test)
-cm_XGB = confusion_matrix(y_test, y_pred_XGB)
-print(cm_XGB)
-print("Accuracy: {}%".format(accuracy_score(y_test, y_pred_XGB)))
+xgb_gs = GridSearchCV(gbc, gbc_params, scoring='accuracy', cv=3)
 
-XGB_accuracies = cross_val_score(xgb, X = X_train, y = y_train, cv=7)
-print("The Avg Accuracy of cross-val-score: {} %".format(XGB_accuracies.mean()*100))
+xgb_gs.fit(X_train, y_train)
 
+print("Best Score: ", xgb_gs.best_score_)
+print("Best parameters: ", xgb_gs.best_params_)
+print("Best model(estimator): ", xgb_gs.best_estimator_)
 #######################################RandomForestClassifier########################################
 
 from sklearn.ensemble import RandomForestClassifier
 
-rfc = RandomForestClassifier(n_estimators=160,criterion='entropy')
+rfc = RandomForestClassifier(random_state=42)
 
-rfc.fit(X_train, y_train)
+print("Avg RandomForest accuracy: ", np.mean(cross_val_score(xgb, X_train, y_train, cv=5)), "%")
 
-y_pred_rfc = rfc.predict(X_test)
-cm_rfc = confusion_matrix(y_test, y_pred_rfc)
-print(cm_rfc)
-print("Accuracy: {}%".format(accuracy_score(y_test, y_pred_rfc)))
+rfc_params = [{             }]
 
-rfc_accuracies = cross_val_score(rfc, X = X_train, y = y_train, cv=7)
-print("The Avg Accuracy of cross-val-score: {} %".format(rfc_accuracies.mean()*100))
+rfc_gs = GridSearchCV(gbc, gbc_params, scoring='accuracy', cv=3)
 
+rfc_gs.fit(X_train, y_train)
+
+print("Best Score: ", rfc_gs.best_score_)
+print("Best parameters: ", rfc_gs.best_params_)
+print("Best model(estimator): ", rfc_gs.best_estimator_)
 ###################################SupportVectorMachine####################################################
 from sklearn.preprocessing import StandardScaler
 
@@ -88,18 +93,18 @@ X_test[:, [1,4]] = sc.transform(X_test[:, [1,4]])
 
 from sklearn.svm import SVC
 
-svc = SVC(kernel='rbf')
+svc = SVC(random_state=42)
+print("Avg SVM accuracy", np.mean(cross_val_score(svc, X_train, y_train, cv=5)), "%")
 
-svc.fit(X_train, y_train)
+svc_params = [{             }]
 
-y_pred_svc = svc.predict(X_test)
-cm_svc = confusion_matrix(y_test, y_pred_svc)
-print(cm_svc)
-print("Accuracy: {}%".format(accuracy_score(y_test, y_pred_svc)))
+svc_gs = GridSearchCV(gbc, gbc_params, scoring='accuracy', cv=3)
 
-svc_accuracies = cross_val_score(svc, X = X_train, y = y_train, cv=7)
-print("The Avg Accuracy of cross-val-score: {} %".format(svc_accuracies.mean()*100))
+svc_gs.fit(X_train, y_train)
 
+print("Best Score: ", svc_gs.best_score_)
+print("Best parameters: ", svc_gs.best_params_)
+print("Best model(estimator): ", svc_gs.best_estimator_)
 ###########################################ModelSaving#################################################
 
 
