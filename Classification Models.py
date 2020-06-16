ac2fcba678fc2c4ae8c1cb8d681334ad85a1cef1
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-06-16 14:58:07 +0300

    new models, fixes

--- Classification Models.py
+++ Classification Models.py
@@ -23,83 +23,73 @@ df_dums = pd.get_dummies(df_model)
 X = df_dums.drop(['Survived','Sex_male', 'Embarked_Southampton'], axis=1).values
 y = df_dums['Survived'].values
 
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
 
 #creating train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 #function for model saving
 def save_model(model, name):
-    filename = name
+    filename = name+".sav"
     pickle.dump(model, open(filename, 'wb'))
- 
+    
+#function for calculating model performance
+def model_performance(filename):
+    loaded_model = pickle.load(open(filename, 'rb'))
+    accuracy = loaded_model.score(X_test, y_test)
+    plot_confusion_matrix(loaded_model, X_test, y_test)
+    return accuracy
+
 ######################################GradientBoostingClassifier###########################################
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
 
-gbc = GradientBoostingClassifier(random_state=42)
-print("Avg GradientBoosting accuracy: ", np.mean(cross_val_score(gbc, X_train, y_train, cv=10)), "%")
+lrc = LogisticRegression(max_iter=2000)
+print("Avg LogisticRegression accuracy: ", np.mean(cross_val_score(lrc, X_train, y_train, cv=3)), "%")
 
-gbc_params = [{'loss':('deviance','exponential'), 'learning_rate': (0.001, 0.01, 0.1), 'n_estimators': range(300,1500,300),
-               'criterion':('friedman_mse', 'mse', 'mae'), 'min_samples_split': range(2,14,4), 'min_samples_leaf': range(2,20,6),
-               'max_features': ('auto', 'sqrt', 'log2', None)}]
+lrc_params = [{'C':(1.0, 2.0, 3.0), 'fit_intercept':(True, False),'solver':('newton-cg', 'lbfgs', 'sag'),
+               'multi_class':('auto', 'ovr', 'multinomial'), 'max_iter':range(6000,8000,500)}]
 
-gbc_gs = GridSearchCV(gbc, gbc_params, scoring='accuracy', cv=3)
+lrc_gs = GridSearchCV(lrc, lrc_params, scoring='accuracy', cv=3)
 
-gbc_gs.fit(X_train, y_train)
+lrc_gs.fit(X_train, y_train)
 
-gbc_estimator = gbc_gs.best_estimator_
+lrc_estimator = lrc_gs.best_estimator_
 
-print("Best Score: ", gbc_gs.best_score_)
-print("Best parameters: ", gbc_gs.best_params_)
-print("Best model(estimator): ", gbc_gs.best_estimator_)
-
-save_model(gbc_estimator, 'gbc_estimator')
+save_model(lrc_estimator, 'lrc_estimator')
+model_performance('lrc_estimator.sav')
 #########################################XGBClassifier#########################################
 from xgboost import XGBClassifier
 
-xgb = XGBClassifier(random_state=42)
-print("Avg XGBoosting accuracy: ", np.mean(cross_val_score(xgb, X_train, y_train, cv=10)), "%")
+xgb = XGBClassifier()
+print("Avg XGBoosting accuracy: ", np.mean(cross_val_score(xgb, X_train, y_train, cv=3)), "%")
 
-xgb_params = [{'eta': (0.05,0.1,0.3,0.5), 'gamma':(0, 4, 8, 12), 'max_depth':(3,6,9), 
-               'subsample':(0.5, 1), 'lambda':(0,1,2), 'alpha':(0,1,2), 
-               'tree_method':('auto','exact'), 
-               'grow_policy':('depthwise','lossguide'),'max_leaves':(0,8,16)}]
+xgb_params = [{'eta': (0.05,0.1,0.3,0.5), 'gamma':(0, 4, 8, 12), 'max_depth':(3,6,9),'subsample':(0.5, 1), 
+               'lambda':(0,1,2), 'alpha':(0,1,2),'tree_method':('auto','exact'), 'learning_rate':(0.001,0.01,0.1),
+               'grow_policy':('depthwise','lossguide'),'max_leaves':(0,8,16), 'n_estimators':range(100,1000,100)}]
 
 xgb_gs = GridSearchCV(xgb, xgb_params, scoring='accuracy', cv=3)
 
 xgb_gs.fit(X_train, y_train)
 
-xgb_estimator = gbc_gs.best_estimator_
+xgb_estimator = xgb_gs.best_estimator_
 
 print("Best Score: ", xgb_gs.best_score_)
 print("Best parameters: ", xgb_gs.best_params_)
 print("Best model(estimator): ", xgb_gs.best_estimator_)
 
 save_model(xgb_estimator, 'xgb_estimator')
-#######################################RandomForestClassifier########################################
-from sklearn.ensemble import RandomForestClassifier
+model_performance('xgb_estimator.sav')
+#########################################GaussianNaiveBaysClassifier######################################
+from sklearn.naive_bayes import GaussianNB
 
-rfc = RandomForestClassifier(random_state=42)
+nbc = GaussianNB()
+print("Avg RandomForest accuracy: ", np.mean(cross_val_score(nbc, X_train, y_train, cv=3)), "%")
 
-print("Avg RandomForest accuracy: ", np.mean(cross_val_score(xgb, X_train, y_train, cv=10)), "%")
+nbc.fit(X_train, y_train)
 
-rfc_params = [{'n_estimators':(100,900,200), 'criterion':('gini', 'entropy'),
-               'max_depth':(3,6,9,None), 'min_samples_split':(2,4,6),
-               'max_features':('auto','sqrt','log2'), 'max_leaf_nodes':(8,16,32)}]
-
-rfc_gs = GridSearchCV(rfc, rfc_params, scoring='accuracy', cv=3)
-
-rfc_gs.fit(X_train, y_train)
-
-rfc_estimator = gbc_gs.best_estimator_
-
-print("Best Score: ", rfc_gs.best_score_)
-print("Best parameters: ", rfc_gs.best_params_)
-print("Best model(estimator): ", rfc_gs.best_estimator_)
-
-save_model(rfc_estimator, 'rfc_estimator')
+save_model(nbc, 'nbc')
+model_performance('nbc.sav')
 ###################################SupportVectorMachine####################################################
 from sklearn.preprocessing import StandardScaler
 
@@ -110,8 +100,8 @@ X_test[:, [1,4]] = sc.transform(X_test[:, [1,4]])
 
 from sklearn.svm import SVC
 
-svc = SVC(random_state=42)
-print("Avg SVM accuracy", np.mean(cross_val_score(svc, X_train, y_train, cv=10)), "%")
+svc = SVC()
+print("Avg SVM accuracy", np.mean(cross_val_score(svc, X_train, y_train, cv=3)), "%")
 
 svc_params = [{'C':(0.5, 1.0, 2.0), 'kernel':('poly','rbf','sigmoid'), 
                'degree':range(3,9,3), 'gamma':('scale','auto')}]
@@ -120,14 +110,17 @@ svc_gs = GridSearchCV(svc, svc_params, scoring='accuracy', cv=3)
 
 svc_gs.fit(X_train, y_train)
 
-svc_estimator = gbc_gs.best_estimator_
+svc_estimator = svc_gs.best_estimator_
 
 print("Best Score: ", svc_gs.best_score_)
 print("Best parameters: ", svc_gs.best_params_)
 print("Best model(estimator): ", svc_gs.best_estimator_)
 
 save_model(svc_estimator, 'svc_estimator')
-###########################################ModelSaving#################################################
+model_performance('svc_estimator.sav')
+
+
+    
 
 
 
