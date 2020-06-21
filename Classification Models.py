727d43bdd3c5642941a52c2f4ecdd39438327fc9
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-06-21 16:18:09 +0300

    Update Classification Models.py

--- Classification Models.py
+++ Classification Models.py
@@ -9,6 +9,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
 #load our train data and test data
 df_train = pd.read_csv('df_train_new.csv')
 
@@ -23,9 +31,6 @@ df_dums = pd.get_dummies(df_model)
 X = df_dums.drop(['Survived','Sex_male', 'Embarked_Southampton'], axis=1).values
 y = df_dums['Survived'].values
 
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
-from sklearn.metrics import plot_confusion_matrix
-
 #creating train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
@@ -34,15 +39,27 @@ def save_model(model, name):
     filename = name+".sav"
     pickle.dump(model, open(filename, 'wb'))
     
-#function for calculating model performance
-def model_performance(filename):
-    loaded_model = pickle.load(open(filename, 'rb'))
-    accuracy = loaded_model.score(X_test, y_test)
-    plot_confusion_matrix(loaded_model, X_test, y_test)
-    return accuracy
+#function for calculating model performance (Scaled Data)
+def model_performance(model, filename):
+    if model == 'Support Vector Machine':
+        loaded_model = pickle.load(open(filename, 'rb'))
+        accuracy = loaded_model.score(scaled_X_test, y_test)
+        plot_confusion_matrix(loaded_model, scaled_X_test, y_test)
+        return accuracy
+    else:
+        loaded_model = pickle.load(open(filename, 'rb'))
+        accuracy = loaded_model.score(X_test, y_test)
+        plot_confusion_matrix(loaded_model, X_test, y_test)
+        return accuracy
 
+#function for feature scaling (Standardization)
+def data_scaler(train_features, test_features, train_cols = [], test_cols = []):
+    sc = StandardScaler()
+    train_features[: , train_cols] = sc.fit_transform(train_features[: , train_cols])
+    test_features[: , test_cols] = sc.transform(test_features[: , test_cols])
+    return train_features, test_features
+    
 ######################################LogisticRegressionClassifier############################################
-from sklearn.linear_model import LogisticRegression
 
 lrc = LogisticRegression(max_iter=2000)
 print("Avg LogisticRegression accuracy: ", np.mean(cross_val_score(lrc, X_train, y_train, cv=3)), "%")
@@ -57,9 +74,9 @@ lrc_gs.fit(X_train, y_train)
 lrc_estimator = lrc_gs.best_estimator_
 
 save_model(lrc_estimator, 'lrc_estimator')
-model_performance('lrc_estimator.sav')
+model_performance('LogisticRegression', 'lrc_estimator.sav')
+
 #########################################KNN########################################################
-from sklearn.neighbors import KNeighborsClassifier
 
 knc = KNeighborsClassifier()
 print("Avg KNeighborsClassifier accuracy: ", np.mean(cross_val_score(knc, X_train, y_train, cv=3)), "%")
@@ -75,45 +92,36 @@ knc_gs.fit(X_train, y_train)
 knc_estimator = knc_gs.best_estimator_
 
 save_model(knc_estimator, 'knc_estimator')
-model_performance('knc_estimator.sav')
+model_performance('K-NearestNeighbors','knc_estimator.sav')
+
 #########################################GaussianNaiveBaysClassifier###########################################
-from sklearn.naive_bayes import GaussianNB
 
 nbc = GaussianNB()
-print("Avg RandomForest accuracy: ", np.mean(cross_val_score(nbc, X_train, y_train, cv=3)), "%")
+print("Avg Gaussian Naive Bayes accuracy: ", np.mean(cross_val_score(nbc, X_train, y_train, cv=3)), "%")
 
 nbc.fit(X_train, y_train)
 
 save_model(nbc, 'nbc')
-model_performance('nbc.sav')
+model_performance('Gaussian Naive Bayes','nbc.sav')
+
 ###################################SupportVectorMachine########################################################
-from sklearn.preprocessing import StandardScaler
 
-sc = StandardScaler()
-
-X_train[:, [1,4]] = sc.fit_transform(X_train[:, [1,4]])
-X_test[:, [1,4]] = sc.transform(X_test[:, [1,4]])
-
-from sklearn.svm import SVC
+scaled_X_train, scaled_X_test = data_scaler(X_train, X_test, [1,4], [1,4])
 
 svc = SVC()
-print("Avg SVM accuracy", np.mean(cross_val_score(svc, X_train, y_train, cv=3)), "%")
+print("Avg SVM accuracy", np.mean(cross_val_score(svc, scaled_X_train, y_train, cv=3)), "%")
 
 svc_params = [{'C':(0.5, 1.0, 2.0), 'kernel':('poly','rbf','sigmoid'), 
                'degree':range(3,9,3), 'gamma':('scale','auto')}]
 
 svc_gs = GridSearchCV(svc, svc_params, scoring='accuracy', cv=3)
 
-svc_gs.fit(X_train, y_train)
+svc_gs.fit(scaled_X_train, y_train)
 
 svc_estimator = svc_gs.best_estimator_
 
-print("Best Score: ", svc_gs.best_score_)
-print("Best parameters: ", svc_gs.best_params_)
-print("Best model(estimator): ", svc_gs.best_estimator_)
-
 save_model(svc_estimator, 'svc_estimator')
-model_performance('svc_estimator.sav')
+model_performance('Support Vector Machine','svc_estimator.sav')
 
 
     
